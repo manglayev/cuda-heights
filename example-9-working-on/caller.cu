f386346c3a308e218eb0d0bfc5b663082048e333
Talgat-qypshaq <talgat.manglayev@gmail.com>
2025-08-07 11:37:23 +0500

    added basic modern C++ transform run on the device and lambda

---
+++ example-9-working-on/caller.cu
@@ -0,0 +1,49 @@
+#include "header.h"
+#include "device_launch_parameters.h"
+#include "cuda_runtime.h"
+
+#include <thrust/execution_policy.h>
+#include <thrust/universal_vector.h>
+#include <thrust/transform.h>
+#include <cstdio>
+
+/*
+    Simple C++ transform run on the device and labmda
+*/
+
+void playing_with_lambda()
+{
+    int x = 10;
+    auto lambda = [=](){ printf("X = %d;\n", x); };
+    printf("x = %d;\n", x);
+    lambda();
+
+    auto lambda2 = [=]() { return x * 3; };
+    int y = lambda2();
+    printf("y = %d;\n", y);
+}
+
+void caller()
+{
+    cout << "caller start;\n";    
+    
+    playing_with_lambda();
+
+    size_t N = 5;
+    thrust::universal_vector<int> c {N};
+    for(int i = 0; i < c.size(); i++)
+    {
+        c[i] = i * 2;
+        printf("%d ", c[i]);
+    }
+    printf("\n");
+
+    thrust::transform(thrust::device, c.begin(), c.end(), c.begin(), [] __host__ __device__ (int d) { return d * 3; });
+
+    for(int i = 0; i < c.size(); i++)
+    {
+        printf("%d ", c[i]);
+    }
+    printf("\n");
+    cout << "caller end;\n";
+}
---
+++ example-9-working-on/main.cpp
@@ -0,0 +1,9 @@
+#include "header.h"
+/* SIMPLE PROGRAM TO CALL A GPU KERNEL WITHOUT ANY ARGUMENTS*/
+int main()
+{
+    cout<<"main start\n";
+    caller();
+    cout<<"main end\n";
+    return 0;
+}
